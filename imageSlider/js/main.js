ad5722f5e63ee54400121d05fcc10dc64eeff8f6
xieyuls <410880162@qq.com>
2017-04-02 11:38:28 +0800

    增加新应用

---
+++ imageSlider/js/main.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Xieyu on 2017/4/2.
+ */
+
+//使用自执行函数隔离作用域
+var Slider=(function () {
+    //记录left的值
+    var moveLeft=0;
+    // 初始化,绑定轮播事件和点击事件
+    function init(){
+        var content=document.getElementById('content');
+        var button1=document.getElementById('moveLeft');
+        //此处为自动轮播
+        setInterval(animate,5000,content,20);
+        //此处为绑定按钮进行点击切换轮播
+        button1.addEventListener('click',function (e) {
+            setTimeout(animate,100,content,20);
+        });
+    }
+    //通过定时器实现轮播的动画效果
+    function animate(content,speed) {
+        if(moveLeft===0){
+            var timer1=setInterval(function () {
+                if(moveLeft>-560) {
+                    moveLeft-=speed;
+                    content.style.left=moveLeft+'px';
+                }
+                else clearInterval(timer1);
+            },10);
+        }else if(moveLeft===-560){
+            var timer2=setInterval(function () {
+                if(moveLeft>-1120) {
+                    moveLeft -= speed;
+                    content.style.left=moveLeft+'px';
+                }
+                else clearInterval(timer2);
+            },10);
+        }else{
+            var timer3=setInterval(function () {
+                if(moveLeft<0){
+                    moveLeft+=2*speed;
+                    content.style.left=moveLeft+'px';
+                }
+                else clearInterval(timer3);
+            },10);
+        }
+    }
+    return{
+        init:init,
+        animate:animate
+    }
+})();
+
+Slider.init();
