ab5388fa8531a1cdab556bfeb5249545347f6e33
xieyuls <410880162@qq.com>
2017-04-02 12:05:23 +0800

    1

--- imageSlider/js/main.js
+++ imageSlider/js/main.js
@@ -21,11 +21,16 @@ var Slider=(function () {
     function animate(content,speed) {
         if(moveLeft===0){
             var timer1=setInterval(function () {
+                moveLeft-=speed;
                 if(moveLeft>-560) {
-                    moveLeft-=speed;
                     content.style.left=moveLeft+'px';
                 }
-                else clearInterval(timer1);
+                else if(Math.abs(moveLeft+560)<speed){
+                    content.style.left=-560+'px';
+                    clearInterval(timer1);
+                }else{
+                    clearInterval(timer1);
+                }
             },10);
         }else if(moveLeft===-560){
             var timer2=setInterval(function () {
@@ -33,15 +38,25 @@ var Slider=(function () {
                     moveLeft -= speed;
                     content.style.left=moveLeft+'px';
                 }
-                else clearInterval(timer2);
+                else if(Math.abs(moveLeft+1120)<speed){
+                    content.style.left=-1120+'px';
+                    clearInterval(timer2);
+                }else{
+                    clearInterval(timer2);
+                }
             },10);
-        }else{
+        }else if(moveLeft===-1120){
             var timer3=setInterval(function () {
                 if(moveLeft<0){
                     moveLeft+=2*speed;
                     content.style.left=moveLeft+'px';
                 }
-                else clearInterval(timer3);
+                else if(Math.abs(moveLeft)<2*speed){
+                    content.style.left=0+'px';
+                    clearInterval(timer3);
+                }else{
+                    clearInterval(timer3);
+                }
             },10);
         }
     }
